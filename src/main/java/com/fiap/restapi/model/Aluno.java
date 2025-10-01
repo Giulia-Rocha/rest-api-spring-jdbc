2de747149f79c74de916ccef50a6eec1b2da7f99
Giulia Barbizan <giuliabarbizan@gmail.com>
2025-10-01 12:11:35 -0300

    add model entities

---
+++ src/main/java/com/fiap/restapi/model/Aluno.java
@@ -0,0 +1,12 @@
+package com.fiap.restapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Aluno {
+    private Long id;
+    private String nome;
+    private String curso;
+}
